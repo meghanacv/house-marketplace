f24aa9dd296b7e7bc6e6647fca409fb1868064d6
Meghana C V <meghana.cvmcvm@gmail.com>
2024-03-20 23:31:30 +0530

    adding project skeleton

---
+++ src/firebase.config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFireStore } from "firebase/firestore";
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB_jX93ZDsQGzlzdWkMPzgU_SdcT9ERsCw",
+  authDomain: "house-marketplace-app-d07b3.firebaseapp.com",
+  projectId: "house-marketplace-app-d07b3",
+  storageBucket: "house-marketplace-app-d07b3.appspot.com",
+  messagingSenderId: "804976261763",
+  appId: "1:804976261763:web:f08f8c9a64e0b2cce339b6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFireStore();
