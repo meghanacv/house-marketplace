421abe3df551b6c4d87fa5a138e1e495bf9339b7
Meghana C V <meghana.cvmcvm@gmail.com>
2024-03-22 14:04:55 +0530

    Added Authentication

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFireStore } from "firebase/firestore";
+import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyB_jX93ZDsQGzlzdWkMPzgU_SdcT9ERsCw",
@@ -12,4 +12,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFireStore();
+export const db = getFirestore();
