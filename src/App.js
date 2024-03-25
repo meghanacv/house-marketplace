cbb4703fa33cc093041484105a63267b9b9e6dcf
Meghana C V <meghana.cvmcvm@gmail.com>
2024-03-25 14:06:05 +0530

    Get & Create Listings

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import "react-toastify/dist/ReactToastify.css";
 import Navbar from "./Components/Navbar";
 import PrivateRoute from "./Components/PrivateRoute";
 import Explore from "./pages/Explore";
+import Category from "./pages/Category";
 import ForgotPassword from "./pages/ForgotPassword";
 import Offers from "./pages/Offers";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import CreateListing from "./pages/CreateListing";
 
 function App() {
   return (
@@ -17,12 +19,14 @@ function App() {
         <Routes>
           <Route path="/" element={<Explore />} />
           <Route path="/offers" element={<Offers />} />
+          <Route path="/category/:categoryName" element={<Category />}/>
           <Route path="/profile" element={<PrivateRoute />}>
             <Route path="/profile" element={<Profile />} />
           </Route>
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/create-listing" element={<CreateListing />} />
         </Routes>
         <Navbar />
       </Router>
--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -14,7 +14,7 @@ import { toast } from "react-toastify";
 import Spinner from "../Components/Spinner";
 import ListingItem from "../Components/ListingItem";
 
-function Offers() {
+function Category() {
   const [listings, setListings] = useState(null);
   const [loading, setLoading] = useState(true);
 
@@ -29,7 +29,7 @@ function Offers() {
         //Create a query
         const q = query(
           listingsRef,
-          where("offer", "==", true),
+          where("type", "==", params.categoryName),
           orderBy("timestamp", "desc"),
           limit(10)
         );
@@ -52,12 +52,14 @@ function Offers() {
     };
 
     fetchListings();
-  }, []);
+  }, [params.categoryName]);
   return (
     <div className="category">
       <header>
         <p className="pageHeader">
-          Offers
+          {params.categoryName === "rent"
+            ? "Places for rent"
+            : "Places for sale"}
         </p>
       </header>
 
@@ -74,10 +76,10 @@ function Offers() {
         </main>
         </>
       ) : (
-        <p>There are no offers as of now!</p>
+        <p>No Listings Found for {params.categoryName}</p>
       )}
     </div>
   );
 }
 
-export default Offers;
+export default Category;
